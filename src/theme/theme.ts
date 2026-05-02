5d656f0bab404fd3d062a62efa842b56d7ec455d
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-05-02 19:06:22 +0500

    building navbar initialised

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,29 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
-export const getTheme = (mode: 'light' | 'dark') => createTheme({
+export const getTheme = (mode: "light" | "dark") =>
+  createTheme({
     palette: {
-        mode,
-        primary: {
-        main: '#1976d2',
-        }, 
-    }
-});
+      mode,
+      ...(mode === "light"
+        ? {
+            primary: { main: "#000000" },
+            background: {
+              default: "#ffffff",
+              paper: "#f5f5f5",
+            },
+            text: {
+              primary: "#000000",
+            },
+          }
+        : {
+            primary: { main: "#ffffff" },
+            background: {
+              default: "#121212",
+              paper: "#1e1e1e",
+            },
+            text: {
+              primary: "#ffffff",
+            },
+          }),
+    },
+  });
