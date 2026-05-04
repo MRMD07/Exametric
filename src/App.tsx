5c723dbc89afe68c1547bc3bdc5a87396a77f561
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-05-04 13:56:44 +0500

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+import Home from "./pages/Home.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
