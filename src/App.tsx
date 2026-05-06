706aaab632efd5fc84d6cdacbc0ba3dbd8f9e88a
Qasim Hassan <qasimhassan1947@gmail.com>
2026-05-06 12:49:15 +0000

    added all pages to routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home.tsx";
 import Nmtc from "./pages/Nmtc.tsx";
 import Nptc from "./pages/Nptc.tsx";
 import Nctc from "./pages/Nctc.tsx";
+import Nbtc from "./pages/Nbtc.tsx";
+import Community from "./pages/Community.tsx";
+import Aitutor from "./pages/Aitutor.tsx";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/nmtc" element={<Nmtc />} />
         <Route path="/nptc" element={<Nptc />} />
         <Route path="/nctc" element={<Nctc />} />
+        <Route path="/nbtc" element={<Nbtc />} />
+        <Route path="/community-resources" element={<Community />} />
+        <Route path="/ai-tutor" element={<Aitutor/>} />
       </Routes>
     </BrowserRouter>
   );
