9f25f61e627b0a32ecec1e9338a70038c54039fb
Qasim Hassan <qasimhassan1947@gmail.com>
2026-05-06 11:59:02 +0000

    added NMTC page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
+import Nmtc from "./pages/Nmtc.tsx";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/nmtc" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
