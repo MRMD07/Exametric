ceb1395e7da22dbfdff71cd4ccdb3c373d6b118b
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-05-09 14:32:09 +0500

    code layout for nptc page

--- src/pages/Nptc.tsx
+++ src/pages/Nptc.tsx
@@ -1,11 +1,28 @@
+import { Box } from "@mui/material";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer"
 
 export default function Nptc(){
     return(
         <>
-            <Navbar/>
-            <Footer/>
+            <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                bgcolor: "background.default",
+                color: "text.primary",
+            }}
+            >
+                    {/* Navbar */}
+                <Navbar/>
+
+                    {/* Main Content */}
+                
+
+                    {/* Footer */}
+                <Footer/>
+            </Box>
         </>
     )
 }
