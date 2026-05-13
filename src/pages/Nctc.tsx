b423f3df2f381c2f719d4c672880bd5fa58f26ca
Qasim Hassan <qasimhassan1947@gmail.com>
2026-05-13 16:29:38 +0000

    working on nctc page

--- src/pages/Nctc.tsx
+++ src/pages/Nctc.tsx
@@ -21,6 +21,14 @@ export default function Nctc(){
 
                     {/* Main Content */}
                 <Box>
+                    <SubjectPageLayout 
+                        title="National Chemistry Talent Contest (NCTC)"
+                        guide= { expertGuide }
+                    resources={[
+                        { label: "Past Papers", content: "https://drive.google.com/drive/folders/1IYY-Q0yAL9OS89fh_CA0VhgMZuhHVpWV?usp=drive_link" },
+                        { label: "Syllabus", content: "https://drive.google.com/file/d/14f91euSH5gd5k7c8ciW88R5-zB7Uq3YQ/view" },
+                    ]}                    
+                    />
                 </Box>
 
                     {/* Footer */}
