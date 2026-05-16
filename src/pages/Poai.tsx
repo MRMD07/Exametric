bc00656928b29a8c5de0a6fede80071414d6f69f
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-05-16 18:56:56 +0500

    added poai page

---
+++ src/pages/Poai.tsx
@@ -0,0 +1,38 @@
+import { Box } from "@mui/material";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import SubjectPageLayout from "../components/SubjectPageLayout";
+import { expertGuide } from "../guides/poai";
+
+export default function Poai(){
+    return(
+        <>
+            <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                bgcolor: "background.default",
+                color: "text.primary",
+            }}
+            >
+                    {/* Navbar */}
+                <Navbar/>
+
+                    {/* Main Content */}
+                <Box>
+                    <SubjectPageLayout 
+                        title="Pakistan Olympiad in Artificial Intelligence (POAI)"
+                        guide= { expertGuide }
+                    resources={[
+                        { label: "Resources", content: "https://youtube.com/playlist?list=PLydJzAOIorUcc7paqgZ5iwxNXqAU7TYwW&si=-GAapy_ePTkEPyoe" },
+                    ]}
+                    />
+                </Box>
+
+                    {/* Footer */}
+                <Footer/>
+            </Box>
+        </>
+    )
+}
