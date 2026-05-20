652fad774081210d5085c2d50bac837c4bafa860
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-05-21 00:20:39 +0500

    finalized community page

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -9,6 +9,8 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
+import { useState } from "react";
+import { supabaseClient } from "./supabase";
 
 interface UploadModalProps {
   open: boolean;
@@ -28,9 +30,46 @@ export default function UploadModal({
   open,
   onClose,
 }: UploadModalProps) {
+
+    const [title, setTitle] = useState("");
+    const [subject, setSubject] = useState("");
+    const [link, setLink] = useState("");
+
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        try{
+            const { error } = await supabaseClient.from('resources').insert([{ title: title, subject: subject, drive_link: link }]);
+
+            if (error) {
+                console.error(
+                    "Upload failed:",
+                    error
+                );
+
+                return;
+            }
+            console.log("Upload successful");
+
+            // Reset fields
+            setTitle("");
+            setSubject("");
+            setLink("");
+
+            // Close modal
+            onClose();
+        }catch (error) {
+            console.error(
+            "Unexpected upload error:",
+            error
+            );
+        } 
+    }
+
   return (
     <Modal open={open} onClose={onClose}>
       <Box
+        component="form"
+        onSubmit={handleSubmit}
         sx={{
           position: "absolute",
 
@@ -66,6 +105,9 @@ export default function UploadModal({
         <TextField
           label="Resource Name"
           fullWidth
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
         />
 
         {/* Subject */}
@@ -76,7 +118,9 @@ export default function UploadModal({
 
           <Select
             label="Subject"
-            defaultValue=""
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            required
           >
             {subjects.map((subject) => (
               <MenuItem
@@ -93,11 +137,14 @@ export default function UploadModal({
         <TextField
           label="Google Drive / Notion Link"
           fullWidth
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+          required
         />
 
         <Button
           variant="contained"
-          onClick={onClose}
+          type="submit"
           sx={{
             borderRadius: "999px",
             height: 46,
